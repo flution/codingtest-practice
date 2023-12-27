9c00814fc268b47438e32c314ccc96dc92a1dd5d
flution <flution221@gmail.com>
2023-12-27 23:55:30 +0900

    프로그래머스 1일 1알고

---
+++ Programmers_practice/23년도 12월/조건에 맞게 수열변환하기.js
@@ -0,0 +1,12 @@
+function solution(arr) {
+    return arr.map(x => {
+      if (x >= 50 && x % 2 === 0) {
+        return x / 2;
+      } else if (x < 50 && x % 2 !== 0) {
+        return x * 2;
+      } else {
+        return x;
+      }
+    });
+  }
+  
