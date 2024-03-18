d4e24f42fdac41ef6b96dc63e75f36c994bb8c69
flution <137801344+flution@users.noreply.github.com>
2024-03-18 09:45:03 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.20 ms, Memory: 31.1 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.cs
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.cs
@@ -3,9 +3,8 @@ using System;
 public class Solution {
     public string solution(string my_string, string overwrite_string, int s) {
         string answer = "";
-
-     answer = my_string.Substring(0, s) + overwrite_string + my_string.Substring(s + overwrite_string.Length);
-
+        answer = my_string.Remove(s, overwrite_string.Length);
+        answer = answer.Insert(s, overwrite_string);
         return answer;
     }
 }
