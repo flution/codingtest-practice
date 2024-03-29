f241dad438f0f8b285280816257e917cac2ff9a7
Elk <137801344+flution@users.noreply.github.com>
2024-03-29 23:09:53 +0900

    [level 0] Title: n의 배수, Time: 0.16 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181937. n의 배수/n의 배수.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution {
+    public int solution(int num, int n) {
+        if (num % n == 0) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
