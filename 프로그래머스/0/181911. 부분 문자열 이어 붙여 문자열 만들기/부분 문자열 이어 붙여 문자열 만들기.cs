c0adf09f661de522d5cac0fb0dedaa77ab1f0752
Elk <137801344+flution@users.noreply.github.com>
2024-05-08 23:44:48 +0900

    [level 0] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 0.93 ms, Memory: 31.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Solution {
+    public string solution(string[] my_strings, int[,] parts) {
+        string answer = "";
+
+            for (int i = 0; i < parts.GetLength(0); i++)
+            {
+                int s = parts[i, 0];
+                int e = parts[i, 1];
+
+                answer += my_strings[i].Substring(s, e - s + 1);
+            }
+
+            return answer;
+    }
+}
